9e2bd5d270a3f1b96bacefbeab6f5cedf613c9b4
Jaime Llanos <jllanosb@unc.edu.pe>
2026-06-05 10:17:43 -0500

    Creando tabla PAis

--- DDL/SQLQuery-002.sql
+++ DDL/SQLQuery-002.sql
@@ -2,23 +2,24 @@
 use master
 --1. Validar existencia base de datos
 if DB_ID ('TURISMOPERU_JLLB') is not null
-	drop database TURISMOPERU_JLLB
-	print 'La base de datos TURISMOPERU_JLLB Eliminada'
+	drop database TURISMOPERU_JLLB;
+	print 'La base de datos TURISMOPERU_JLLB Eliminada';
 go
 
--- 2. Crear la base de datoa
-CREATE DATABASE TURISMOPERU_JLLB
-print 'Base de datos TURISMOPERU_JLLB creada correctamente'
+-- 2. Crear la base de datos
+CREATE DATABASE TURISMOPERU_JLLB;
+print 'Base de datos TURISMOPERU_JLLB creada correctamente';
 go
 
 --3. Usar la Base de datos TURISMOPERU_JLLB
 use TURISMOPERU_JLLB;
-print 'Base de Datos TURISMOPERU_JLLB Seleccionada'
+print 'Base de Datos TURISMOPERU_JLLB Seleccionada';
 go
 
 --4. Crear Esquema de Tablas
-CREATE SCHEMA JLLB
-print 'Se creó el Esquema JLLB'
+CREATE SCHEMA JLLB;
+GO
+print 'Se creï¿½ el Esquema JLLB';
 go
 
 --5. Crear Estructura de la Tablas
@@ -51,3 +52,18 @@ HABITACION
 --ALIMENTACION
 */
 
+--Tabla Pais
+if OBJECT_ID ('jllb.pais', 'U') is not null
+begin
+	DROP Table jllb.pais;
+	print 'Tabla pais Eliminada'
+end
+go
+
+CREATE TABLE JLLB.PAIS (
+	id_pais int identity(1,1) primary key,
+	nombrepais nvarchar(100) not null UNIQUE,
+	codigo_iso char(3) not null UNIQUE --e.g PER, USA, ESP, COL 
+);
+print 'Tabla Pais Creada Correctamente';
+GO
